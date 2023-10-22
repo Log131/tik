4b26c8629c30f2b50aea0397844456e5f320050f
Log131 <87196980+Log131@users.noreply.github.com>
2023-10-23 01:28:05 +0300

    Add files via upload

---
+++ times5.py
@@ -0,0 +1,57 @@
+import schedule
+
+import datetime
+
+from videos import bot
+import asyncio
+import time
+import aiosqlite
+from aiogram.types import ChatMemberStatus
+
+
+
+
+
+
+
+async def tests():
+    async with aiosqlite.connect('teleg.db') as tc:
+        async with tc.execute('SELECT * FROM users') as f:
+            s_ = await f.fetchall()
+    
+
+
+
+
+    dates = datetime.datetime.now().strftime('%Y-%m-%d %H-%M')
+    for i in s_:
+        s = await bot.get_chat_member(chat_id=-1001791109996, user_id=i[0])
+        if s.status in [ChatMemberStatus.MEMBER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR] and i[1] >= dates and i[2] == 0:
+            try:
+
+
+
+                async with aiosqlite.connect('teleg.db') as tc:
+                    await tc.execute('UPDATE users SET sends = 5 WHERE userid = ?', (i[0],))
+                    await tc.commit()
+                await bot.send_message(chat_id=i[0], text='Спасибо за подписку \n https://youtu.be/iNwjdSx1VdY?si=1B0Ebk-vKEp4dyI2')
+            except:
+                pass
+        
+
+
+
+
+
+def funcs_():
+    f = asyncio.get_event_loop()
+    f.run_until_complete(tests())
+
+schedule.every(5).seconds.do(funcs_)
+
+
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(5)
---
+++ videos.py
@@ -0,0 +1,57 @@
+from aiogram import Dispatcher,Bot,executor,types
+
+from aiogram.types import ChatMemberStatus, InlineKeyboardButton, InlineKeyboardMarkup
+
+import datetime
+import aiosqlite
+import asyncio
+
+
+
+
+
+
+
+token = '6937427392:AAF8ZtTp-FBeODozbOUfsGNWo5tzKj2xSF0'
+
+
+bot = Bot(token=token)
+dp = Dispatcher(bot=bot)
+
+
+
+
+
+async def datas_():
+    async with aiosqlite.connect('teleg.db') as tc:
+        await tc.execute('CREATE TABLE IF NOT EXISTS users(userid,dates,sends)')
+        await tc.commit()
+
+@dp.message_handler(commands=['start'])
+async def state_(msg: types.Message):
+    row = InlineKeyboardMarkup()
+    
+    rows = InlineKeyboardButton(text='Перейти на канал', url='https://t.me/kaif_works')
+    
+    
+    
+    row.add(rows)
+    dates = (datetime.datetime.now() + datetime.timedelta(minutes=5)).strftime('%Y-%m-%d %H:%M')
+    async with aiosqlite.connect('teleg.db') as tc:
+        await tc.execute('INSERT OR IGNORE INTO users(userid,dates,sends) VALUES (?,?,?)', (msg.from_user.id,dates,0))
+        
+        await tc.commit()
+
+    try:
+        await msg.answer('Привет, Для просмотра второй части из ТикТока, нужны быть подписанным на канал по заработку на написании отзывов @kaif_work', reply_markup=row)
+    except:
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    f = asyncio.get_event_loop()
+    f.run_until_complete(datas_())
+    executor.start_polling(dp, skip_updates=True)
